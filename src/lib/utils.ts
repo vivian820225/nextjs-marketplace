1b305ece92384b3c49ba9654a593a530a9747837
VC <vivian820225@gmail.com>
2024-03-14 11:29:33 +0800

    Add: cart

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'TWD' | 'JPY' | 'USD'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) {
+  const { currency = 'TWD', notation = 'compact' } = options
+
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price
+
+  return new Intl.NumberFormat('zh-Hant', {
+    style: 'currency',
+    currency,
+    notation,
+  }).format(numericPrice)
 }
