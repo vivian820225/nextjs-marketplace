08df0cb6a72777d4cde58ec94006c5851c0afb52
VC <vivian820225@gmail.com>
2024-03-19 10:49:00 +0800

    Add: collections - products, productFiles, media, orders

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,13 +1,22 @@
 import path from 'path'
+import dotenv from 'dotenv'
 import { buildConfig } from 'payload/config'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { Users } from './collections/Users'
+import { Products } from './collections/Products/Products'
+import { Media } from './collections/Media'
+import { ProductFiles } from './collections/ProductFiles'
+import { Orders } from './collections/Orders'
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+})
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: '/dashboard',
   },
