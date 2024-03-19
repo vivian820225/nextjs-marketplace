777b2e22fbec101fa88e6402c907687730fb9d18
VC <vivian820225@gmail.com>
2024-03-19 11:24:42 +0800

    Fix: password validator rule

--- src/lib/validators/account-credentials-validator.ts
+++ src/lib/validators/account-credentials-validator.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const AuthCredentialsValidator = z.object({
   email: z.string().email(),
-  password: z.string().min(8, {
+  password: z.string().min(6, {
     message: 'Password must be at least 6 characters long.',
   }),
 })
