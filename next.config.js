70d534b32bc09502af64a9b896ea896239ef5c18
VC <vivian820225@gmail.com>
2024-03-27 22:17:20 +0800

    Chore: update next.config

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_SERVER_URL,
+        hostname: 'foxtrove.onrender.com',
       },
     ],
   },
